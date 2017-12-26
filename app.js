2b69c392630caad7c450ecfdd427a25c89c8e0ad
Carlos Grijalva <grijalva.carlos@gmail.com>
2017-12-26 15:59:02 -0500

    Add homepage and basic styling

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.render('homepage');
+});
+
+app.listen(3000, () => {
+  console.log('app now listening for requests on port 3000');
+});
+
